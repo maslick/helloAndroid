6c76701f1c4ee90243e04c22481da874615e22f6
Pavel Maslov <pavel.maslov@ijs.si>
2016-11-15 11:47:02 +0100

    * toggle button

--- app/src/main/java/com/example/maslick/helloworld/HahaActivity.java
+++ app/src/main/java/com/example/maslick/helloworld/HahaActivity.java
@@ -1,29 +1,67 @@
 package com.example.maslick.helloworld;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 
 /**
  * Created by maslick on 14.11.2016.
  */
 
 public class HahaActivity extends AppCompatActivity {
+    private ProgressBar progressBar;
+    private Button button;
+    private boolean isSpinning = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.haha);
+        button = (Button) findViewById(R.id.toggleBtn);
+        button.setText("Hide");
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+    }
 
-        Button button = (Button) findViewById(R.id.showMainBtn);
-        button.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View view) {
-                Intent nextScreen = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(nextScreen);
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if (isSpinning) {
+                progressBar.setVisibility(View.INVISIBLE);
+                button.setText("Show");
+                isSpinning = !isSpinning;
             }
-        });
+            else {
+                progressBar.setVisibility(View.VISIBLE);
+                button.setText("Hide");
+                isSpinning = !isSpinning;
+            }
+        }
+    };
+
+    public void toggleBtn(View view) {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                long futuretime = System.currentTimeMillis() + 10;
+                while (System.currentTimeMillis() < futuretime) {
+                    synchronized (this) {
+                        try {
+                            wait(futuretime - System.currentTimeMillis());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                handler.sendEmptyMessage(0);
+            }
+        };
+
+        Thread myThread = new Thread(runnable);
+        myThread.start();
     }
 }
