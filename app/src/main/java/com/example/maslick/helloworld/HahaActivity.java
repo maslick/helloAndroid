d757e11b92b412db8f21364da33155140a9ae9ce
Pavel Maslov <pavel.maslov@ijs.si>
2016-11-16 11:55:12 +0100

    added toaster and notifications

--- app/src/main/java/com/example/maslick/helloworld/HahaActivity.java
+++ app/src/main/java/com/example/maslick/helloworld/HahaActivity.java
@@ -1,12 +1,18 @@
 package com.example.maslick.helloworld;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.NotificationCompat;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 /**
  * Created by maslick on 14.11.2016.
@@ -17,13 +23,35 @@ public class HahaActivity extends AppCompatActivity {
     private Button button;
     private boolean isSpinning = true;
 
-    @Override
+    NotificationCompat.Builder notification;
+    private static int uniqueID = 12345;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.haha);
         button = (Button) findViewById(R.id.toggleBtn);
         button.setText("Hide");
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        // notification
+        notification = new NotificationCompat.Builder(this);
+        notification.setAutoCancel(true);
+        notifyy("Heartman", "You opened settings activity", "readme");
+    }
+
+    private void notifyy(String title, String text, String ticker) {
+        notification.setSmallIcon(R.mipmap.ic_launcher);
+        notification.setTicker(ticker);
+        notification.setWhen(System.currentTimeMillis());
+        notification.setContentTitle(title);
+        notification.setContentText(text);
+
+        Intent intent = new Intent(this, HahaActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        notification.setContentIntent(pendingIntent);
+
+        NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        nm.notify(uniqueID++, notification.build());
     }
 
 
@@ -34,11 +62,15 @@ public class HahaActivity extends AppCompatActivity {
                 progressBar.setVisibility(View.INVISIBLE);
                 button.setText("Show");
                 isSpinning = !isSpinning;
+                toast("not spinning...");
+                notifyy("Heartman", "not spinning", "not spinning...");
             }
             else {
                 progressBar.setVisibility(View.VISIBLE);
                 button.setText("Hide");
                 isSpinning = !isSpinning;
+                toast("spinning...");
+                notifyy("Heartman", "spinning", "spinning...");
             }
         }
     };
@@ -50,5 +82,11 @@ public class HahaActivity extends AppCompatActivity {
 
         Thread myThread = new Thread(runnable);
         myThread.start();
+    }
+
+    private void toast(String text) {
+        Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.BOTTOM|Gravity.CENTER, 0, 20);
+        toast.show();
     }
 }
